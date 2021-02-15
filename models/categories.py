1388c508507ceb4be1f9506db146033183bc38ea
Kreyvi <tankoed@yandex.ru>
2021-02-15 23:07:30 +0300

    started models creation

---
+++ models/categories.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Categories(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    slug = models.SlugField(
+        max_length=50,
+        blank=True,
+        null=False,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Categories, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'"{self.title}"'
