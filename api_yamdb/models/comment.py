c5c10b77a5bcbf80324ea4e2f713ac94b2859ae9
Kreyvi <tankoed@yandex.ru>
2021-02-25 16:40:38 +0300

    verbose_names added except Users

--- api_yamdb/models/comment.py
+++ api_yamdb/models/comment.py
@@ -10,20 +10,27 @@ class Comment(models.Model):
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        verbose_name='Review'
     )
     text = models.TextField(
-        max_length=300
+        max_length=300,
+        verbose_name='Review text'
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        verbose_name='Review author'
     )
     pub_date = models.DateTimeField(
-        'Дата публикации',
+        verbose_name='Date published',
         auto_now_add=True
     )
 
     def __str__(self):
         return f'"{self.text}"'
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
--- api_yamdb/models/review.py
+++ api_yamdb/models/review.py
@@ -9,29 +9,35 @@ class Review(models.Model):
     title = models.ForeignKey(
         Title,
         on_delete=models.CASCADE,
-        related_name='reviews'
+        related_name='reviews',
+        verbose_name='Review title'
     )
     score = models.PositiveSmallIntegerField(
         validators=[
             MinValueValidator(1),
             MaxValueValidator(10)
-        ]
+        ],
+        verbose_name='Rating'
     )
     text = models.TextField(
-        max_length=300
+        max_length=300,
+        verbose_name='Review text'
     )
     author = models.ForeignKey(
         MyUser,
         on_delete=models.CASCADE,
-        related_name='reviews'
+        related_name='reviews',
+        verbose_name='Review author'
     )
     pub_date = models.DateTimeField(
-        'Дата публикации',
+        verbose_name='Date published',
         auto_now_add=True
     )
 
     class Meta:
-        unique_together = ['author', 'title']
+        unique_together = ['author', 'title', ]
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
 
     def __str__(self):
         return f'"{self.text}"'
