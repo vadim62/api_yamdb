880dc65535133de360a156194edeb6f62b29dc75
Вадим <vadim624790@mail.ru>
2021-02-26 15:02:12 +0500

    fix comment

--- api_yamdb/views/CommentViewSet.py
+++ api_yamdb/views/CommentViewSet.py
@@ -17,5 +17,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         return review.comments.all().order_by('id')
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id, title__id=title_id)
         serializer.save(author=self.request.user, review=review)
