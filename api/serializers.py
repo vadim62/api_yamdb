c5010f2076f4285a68f553abe3d705a7878bd0e9
Вадим <vadim624790@mail.ru>
2021-02-27 16:11:24 +0500

    small fix

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .models import Category, Comment, Genre, Review, Title
